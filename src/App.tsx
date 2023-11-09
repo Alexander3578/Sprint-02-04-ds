176d492b7d14a683ad82143dfc587be04a281b07
Alexander Stasyuk <predator12007@mail.ru>
2023-11-09 03:56:50 +0300

    HW1 was done!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import s from './s1-main/App.module.css'
+import HW1 from './s2-homeworks/hw01/HW1';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+    return (
+        <div className={s.App}>
+            <HW1/>
+            {/*раскомментировать по ходу выполнения*/}
+            {/*<HW2/>*/}
+            {/*<HW3/>*/}
+            {/*<HW4/>*/}
+
+            {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
+            {/*<HW5/>*/}
+        </div>
+    )
 }
 
-export default App;
+export default App
